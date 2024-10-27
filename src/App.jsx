ecaf4c0b1beea625f14e0fdcc77b69f93860e498
NachoBastianelli <nachobastianelli2003@gmail.com>
2024-10-27 14:26:55 -0300

    Creacion de rutas dinamicas y componente notFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import CardItem from "./components/cardItem/CardItem";
 import Item from "./components/item/Item";
 import Login from "./components/login/Login";
+import NotFound from "./components/notFound/NotFound";
 import Dashboard from "./dashboard/Dashboard";
 import Header from "./header/Header";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/home",
+      element: <Dashboard />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+    {
+      path: "/",
+      element: <Navigate to="/home" replace />,
+    },
+  ]);
   return (
     <>
-      <Dashboard />
+      <RouterProvider router={router} />
     </>
   );
 }
